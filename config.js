1748af8df592f308207269490dac080a22d8a2de
AutoFTbot <151357338+AutoFTbot@users.noreply.github.com>
2024-11-28 13:02:27 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+   // config.js
+   module.exports = {
+    cloudflare: {
+        apiKey: '25f8263bebf7d631aca3f0c3b23c8ca560a77',
+        email: 'aginazharmhlutpi14@gmail.com',
+        zoneId: 'f77aa1fdacada2a494d8cbe810bbb832'
+    },
+    admin: {
+        username: 'agin', // ubah ini
+        password: 'Mantap123!'  // ubah ini
+    }
+};
